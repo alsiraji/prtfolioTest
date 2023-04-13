5f231eb74b4216162fd363e0071a035161cc5020
alsiraji <m.a.alseraji@gmail.com>
2023-04-13 07:54:21 +0100

    Initial Commit

---
+++ prtfolioTest/prtfolioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  prtfolioTestApp.swift
+//  prtfolioTest
+//
+//  Created by Mohammed Alsiraji on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct prtfolioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
